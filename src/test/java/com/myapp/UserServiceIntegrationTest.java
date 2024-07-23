9e779891f6399963e33bb896e931497d5b2c8b3e
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 06:53:14 -0400

    Update UserServiceIntegrationTest.java

--- src/test/java/com/myapp/UserServiceIntegrationTest.java
+++ src/test/java/com/myapp/UserServiceIntegrationTest.java
@@ -7,8 +7,10 @@ import com.myapp.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class UserServiceIntegrationTest {
 
     @Autowired
