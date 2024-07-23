bebfe335648f6ae5a772c2687e8e1acb08328079
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 07:07:45 -0400

    Update UserRepositoryIntegrationTest.java

--- src/test/java/com/myapp/UserRepositoryIntegrationTest.java
+++ src/test/java/com/myapp/UserRepositoryIntegrationTest.java
@@ -2,8 +2,8 @@ package com.myapp;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.myapp.model.User;
-import com.myapp.repository.UserRepository;
+import com.myapp.User;
+import com.myapp.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
