e73cddf840515a9e06e86ae32fe506844ec4604e
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 16:32:41 -0400

    Create UserServiceImpl.java

---
+++ src/main/java/com/myapp/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.myapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
