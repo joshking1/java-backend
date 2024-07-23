b7cec52702b6d8de266c83f9f4aa345f5b4b8fb9
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 16:20:42 -0400

    Update UserService.java

--- src/main/java/com/myapp/UserService.java
+++ src/main/java/com/myapp/UserService.java
@@ -1,26 +1,9 @@
 package com.myapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
-import java.util.List;
-
-@Service
-public class UserService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    public User getUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
-    }
-
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    public void saveUser(User user) {
-        userRepository.save(user);
-    }
+public interface UserService {
+    User findUserById(Long id);
 }
+
 
