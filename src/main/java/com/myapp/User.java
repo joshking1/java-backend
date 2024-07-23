dc759ba345d25a57fb318e04af279d930d47c526
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 07:04:08 -0400

    Update User.java

--- src/main/java/com/myapp/User.java
+++ src/main/java/com/myapp/User.java
@@ -7,11 +7,44 @@ import javax.persistence.Id;
 
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String username;
-    private String password;
+    private String name;
+    private String email;
 
-    // Getters and Setters
+    // Constructors, getters, and setters
+
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
