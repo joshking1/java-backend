bcc894b5369285b7bf9ad033f613881dae077782
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 06:53:49 -0400

    Update UserRepositoryIntegrationTest.java

--- src/test/java/com/myapp/UserRepositoryIntegrationTest.java
+++ src/test/java/com/myapp/UserRepositoryIntegrationTest.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class UserRepositoryIntegrationTest {
 
     @Autowired
@@ -27,4 +29,3 @@ public class UserRepositoryIntegrationTest {
         assertThat(found.getName()).isEqualTo(user.getName());
     }
 }
-
