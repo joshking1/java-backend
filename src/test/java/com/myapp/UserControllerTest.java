96a1126144d4ff104db4f2622edcca7f90a483f2
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 06:46:31 -0400

    Update and rename UserRepositoryTest.java to UserControllerTest.java

---
+++ src/test/java/com/myapp/UserControllerTest.java
@@ -0,0 +1,46 @@
+package com.myapp;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.myapp.controller.UserController;
+import com.myapp.service.UserService;
+import com.myapp.model.User;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
+        User user = new User("john", "john@example.com");
+        List<User> allUsers = Arrays.asList(user);
+
+        given(userService.getAllUsers()).willReturn(allUsers);
+
+        mockMvc.perform(get("/users")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(1)))
+            .andExpect(jsonPath("$[0].name", is(user.getName())));
+    }
+}
+
