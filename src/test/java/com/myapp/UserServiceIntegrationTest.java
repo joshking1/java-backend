0d0e39d90104d9244acadc05d7dc177eb7d94537
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 06:48:44 -0400

    Create UserServiceIntegrationTest.java

---
+++ src/test/java/com/myapp/UserServiceIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.myapp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.myapp.model.User;
+import com.myapp.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceIntegrationTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void whenValidUserId_thenUserShouldBeFound() {
+        User user = new User("john", "john@example.com");
+        userService.saveUser(user);
+
+        User found = userService.getUserById(user.getId());
+        assertThat(found.getName()).isEqualTo(user.getName());
+    }
+}
