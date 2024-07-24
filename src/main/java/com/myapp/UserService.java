6538d191400b2d27b0c54c8b7362f10f38ac17a0
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 22:59:40 -0400

    Update UserService.java

--- src/main/java/com/myapp/UserService.java
+++ src/main/java/com/myapp/UserService.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public interface UserService {
     List<User> getAllUsers();
-    User saveUser(User user);
-    User getUserById(Long id);
+    User findUserById(long id);
 }
+
