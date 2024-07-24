01c27dd0ca972790e7d32f19d5e97699b4534e94
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-23 22:17:50 -0400

    Update UserRepositoryIntegrationTest.java

--- src/test/java/com/myapp/UserRepositoryIntegrationTest.java
+++ src/test/java/com/myapp/UserRepositoryIntegrationTest.java
@@ -1,17 +1,13 @@
-package com.myapp;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @SpringJUnitConfig
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Transactional
 public class UserRepositoryIntegrationTest {
 
     @Autowired
